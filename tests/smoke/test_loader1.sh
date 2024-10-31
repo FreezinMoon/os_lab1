cbb36b4046abfaafdc822403e8054360c52bbee2
Petr Gumennik <mr.gumennik@niuitmo.ru>
2024-10-31 22:49:31 +0300

    ss

--- tests/smoke/test_loader1.sh
+++ tests/smoke/test_loader1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running smoke tests for loader1"
-./loader1/regular/loader1_regular_opt || exit 1
-./loader1/regular/loader1_regular_nonopt || exit 1
-./loader1/parallel/loader1_parallel || exit 1
+./loader1/regular/loader1_regular_opt Makefile 5|| exit 1
+./loader1/regular/loader1_regular_nonopt Makefile 5|| exit 1
+./loader1/parallel/loader1_parallel Makefile 5|| exit 1
 echo "Loader1 smoke tests passed."
--- tests/smoke/test_loader2.sh
+++ tests/smoke/test_loader2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running smoke tests for loader2"
-./loader2/regular/loader2_regular_opt || exit 1
-./loader2/regular/loader2_regular_nonopt || exit 1
-./loader2/parallel/loader2_parallel || exit 1
+./loader2/regular/loader2_regular_opt 10|| exit 1
+./loader2/regular/loader2_regular_nonopt 10|| exit 1
+./loader2/parallel/loader2_parallel 10|| exit 1
 echo "Loader2 smoke tests passed."
