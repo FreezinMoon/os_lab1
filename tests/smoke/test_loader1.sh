068ef34bc55bf2bf61dcad707d02b7e1ecf8de23
Petr Gumennik <mr.gumennik@niuitmo.ru>
2024-11-01 14:09:34 +0300

    a

--- tests/smoke/test_loader1.sh
+++ tests/smoke/test_loader1.sh
@@ -2,5 +2,5 @@
 echo "Running smoke tests for loader1"
 ./loader1/regular/loader1_regular_opt Makefile 5|| exit 1
 ./loader1/regular/loader1_regular_nonopt Makefile 5|| exit 1
-./loader1/parallel/loader1_parallel Makefile 5|| exit 1
+./loader1/parallel/loader1_parallel Makefile 5 5|| exit 1
 echo "Loader1 smoke tests passed."
